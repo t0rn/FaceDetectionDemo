a6b5fcb65274d1de0cce66124bb53765d54b90da
Alexey Ivanov <aleksey.ivanov1@gmail.com>
2024-02-07 16:34:08 +0100

    Initial Commit

---
+++ FaceDetectionDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceDetectionDemo
+//
+//  Created by Alexey Ivanov on 7/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
